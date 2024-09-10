c39d96f92abe2ae0925e80aea510218ad0632336
gaurav parajuli <gparajuli.1997@gmail.com>
2024-09-10 22:49:29 +0545

    implemented load_from_coco and persist_to_yolo method in Annotations class

--- pybboxes/annotations/base.py
+++ pybboxes/annotations/base.py
@@ -36,9 +36,7 @@ class Annotations:
     def names_mapping(self):
         return {name: id_ for id_, name in enumerate(self._class_names)}
 
-    def __getitem__(
-        self, subscript: Union[int, List[int], slice]
-    ) -> Union[Annotation, List[Annotation]]:
+    def __getitem__(self, subscript: Union[int, List[int], slice]) -> Union[Annotation, List[Annotation]]:
         if isinstance(subscript, list):
             return [self[i] for i in subscript]
         else:
@@ -59,55 +57,51 @@ class Annotations:
         json_path : str
             provide path to coco annotation  file in json format
         """
+        if self._annotation_type != 'coco':
+            raise ValueError(f'this instance of Annotations can only process {self._annotation_type} annotation file(s)')
+
         if not os.path.exists(json_path):
             raise FileNotFoundError(f"{json_path} doesn't exists")
         
         coco = COCO(json_path)
 
-        def load_image_metadata(img_id: int):
-            return coco.loadImgs(img_id)[0] 
-
-        def augment_annotation_with_image_metadata(annotation: dict):
-            img_id = annotation['image_id']
-            annotation['image'] = load_image_metadata(img_id)
-            return annotation
-
         categories = coco.loadCats(coco.getCatIds())
-        categories = [category['name'] for category in categories] # we just need the names
-        self._class_names = categories
+        self._class_names = [category['name'] for category in categories] # we just need the names
 
-        annotations = coco.loadAnns(coco.getAnnIds())
-        annotations = map(augment_annotation_with_image_metadata, annotations) # we will need image metadata for image file name and dimensions
+        for ann_id in coco.getAnnIds():
+            ann = coco.loadAnns(ann_id)[0]
+            img = coco.loadImgs(ann['image_id'])[0]
 
-        for annotation in annotations:
-            ann = Annotation(
-                boxes=[BoundingBox.from_coco(*annotation['bbox'], image_size=(annotation['image']['width'], annotation['image']['height']))],
-                label_id=annotation['category_id'],
-                label_name=self.id2label(annotation['category_id']),
-                annotation_type=self._annotation_file_type,
-                associated_image_name=annotation['image']['file_name'],
-                annotation_id=annotation['id']
+            bbox = BoundingBox.from_coco(*ann['bbox'], image_size=(img['width'], img['height']))
+
+            annotation = Annotation(
+                boxes=[bbox],
+                label_id=ann['category_id'],
+                label_name=self.id2label(ann['category_id']),
+                annotation_type='coco',
+                associated_image_name=img['file_name'],
+                annotation_id=ann['id'],
+                image_width=img['width'],
+                image_height=img['height']
             )
-            
-            self._objects.append(ann)
-    
+
+            self._objects.append(annotation)
+
     def persist_as_yolo(self, export_dir: str):
         os.makedirs(export_dir, exist_ok=True)
         for annotation in self._objects:
 
-            filename = os.path.splitext(annotation.associated_image_name)[0]
-            filepath = os.path.join(export_dir, f'{filename}.txt')
+            filename = f'{os.path.splitext(annotation.associated_image_name)[0]}.txt'
+            filepath = os.path.join(export_dir, filename)
 
-            writing_mode = 'a' if os.path.exists(filepath) else 'w' # some file may contain multiple bounding boxes, in such case, use append prevent over write
-            with open(filepath, mode=writing_mode) as f:
-                bboxes = annotation.boxes[0]
-                bboxes = bboxes.to_yolo() # convert to yolo format
-                bboxes = bboxes.raw_values
-                bboxes = [f'{x:.4f}' for x in bboxes] # convert from float to string and round to 4 decimal places
-                bboxes.insert(0, str(annotation.label_id)) # append the class label at the beginning
-                bboxes = ' '.join(bboxes) # this represents annotation for a single line
-                f.write(f'{bboxes}\n')
+            with open(filepath, mode='a') as f:
+                for box in annotation.boxes:
+                    yolo_box = box.to_yolo().raw_values
+                    yolo_box = [f'{x:.4f}' for x in yolo_box]
+                    yolo_box.insert(0, str(annotation.label_id)) # append class/label id at the beginning
+                    f.write(f"{' '.join(yolo_box)}\n")
 
 if __name__ == "__main__":
-    coco = Annotations(annotation_file_type='coco')
+    coco = Annotations(annotation_type='coco')
     coco.load_from_coco(json_path='Images/annotations_coco.json')
+    coco.persist_as_yolo(export_dir='yolo_test')
