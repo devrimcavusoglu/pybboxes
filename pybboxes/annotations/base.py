1e9b9f73b0a95ed8a3c711a03a63092b9892ed19
gaurav parajuli <gparajuli.1997@gmail.com>
2024-09-11 11:25:58 +0545

    annotations are now grouped by imagename in the internal representations of Annotations class. required updates also made to load_from_coco and persist_as_yolo methods

--- pybboxes/annotations/base.py
+++ pybboxes/annotations/base.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import List, Tuple, Union
+from typing import List, Union, Dict
+import xml.etree.ElementTree as ET
 import os
 
 import numpy as np
@@ -12,12 +13,11 @@ from pybboxes.boxes import BoundingBox
 @dataclass
 class Annotation:
     # https://www.immersivelimit.com/tutorials/create-coco-annotations-from-scratch
-    boxes: List[BoundingBox] # in the same annotation format/type
+    box: BoundingBox
     label_id: int
     label_name: str = None
     annotation_id: int = None
     annotation_type: str = None
-    associated_image_name:str = None
     segmentations: List[int] = None
     image_width: int = None
     image_height: int = None
@@ -30,17 +30,17 @@ class Annotations:
         
         self._annotation_type = annotation_type
         self._class_name: List[str] = []
-        self._objects: List[Annotation] = []
+        self._objects: dict[str, List[Annotation]] = dict()
 
     @property
     def names_mapping(self):
         return {name: id_ for id_, name in enumerate(self._class_names)}
 
-    def __getitem__(self, subscript: Union[int, List[int], slice]) -> Union[Annotation, List[Annotation]]:
-        if isinstance(subscript, list):
-            return [self[i] for i in subscript]
-        else:
-            return self._objects[subscript]
+    # def __getitem__(self, subscript: Union[int, List[int], slice]) -> Union[Annotation, List[Annotation]]:
+    #     if isinstance(subscript, list):
+    #         return [self[i] for i in subscript]
+    #     else:
+    #         return self._objects[subscript]
 
     def label2id(self, name: str):
         return self.names_mapping[name]
@@ -71,37 +71,97 @@ class Annotations:
         for ann_id in coco.getAnnIds():
             ann = coco.loadAnns(ann_id)[0]
             img = coco.loadImgs(ann['image_id'])[0]
+            associated_img_filename = img['file_name']
 
             bbox = BoundingBox.from_coco(*ann['bbox'], image_size=(img['width'], img['height']))
 
             annotation = Annotation(
-                boxes=[bbox],
+                box=bbox,
                 label_id=ann['category_id'],
                 label_name=self.id2label(ann['category_id']),
                 annotation_type='coco',
-                associated_image_name=img['file_name'],
                 annotation_id=ann['id'],
                 image_width=img['width'],
                 image_height=img['height']
             )
 
-            self._objects.append(annotation)
+            # if the entry for this file already exists
+            if associated_img_filename in self._objects:
+                self._objects[associated_img_filename].append(annotation)
+            else:
+                self._objects[associated_img_filename] = [annotation]
 
     def persist_as_yolo(self, export_dir: str):
         os.makedirs(export_dir, exist_ok=True)
-        for annotation in self._objects:
+        for image_name in self._objects.keys():
 
-            filename = f'{os.path.splitext(annotation.associated_image_name)[0]}.txt'
+            filename = f'{os.path.splitext(image_name)[0]}.txt'
             filepath = os.path.join(export_dir, filename)
 
             with open(filepath, mode='a') as f:
-                for box in annotation.boxes:
-                    yolo_box = box.to_yolo().raw_values
+                for annotation in self._objects[image_name]:
+                    yolo_box = annotation.box.to_yolo().raw_values
                     yolo_box = [f'{x:.4f}' for x in yolo_box]
                     yolo_box.insert(0, str(annotation.label_id)) # append class/label id at the beginning
                     f.write(f"{' '.join(yolo_box)}\n")
 
+    def persist_as_voc(self, export_dir: str, n_channels: int=3):
+        os.makedirs(export_dir, exist_ok=True)
+        for annotation in self._objects:
+            filename = os.path.splitext(annotation.associated_image_name)[0] + '.xml'
+            filepath = os.path.join(export_dir, filename)
+
+            # if the xml file aready exists, we just have to append the bounding box in the existing xml file
+            # this is because given image annotation may contain multiple bounding boxes
+            # but we will make a assumption that if annotation file already exits, every relevant metadata
+            # already exists, so we are just going to append the bouding box in the xml file
+
+            if os.path.exists(filepath):
+                tree = ET.parse(filepath)
+                root = tree.getroot()
+
+                obj = ET.SubElement(root, "object")
+                ET.SubElement(obj, "name").text = annotation.label_name
+                ET.SubElement(obj, "pose").text = "Unspecified"
+                ET.SubElement(obj, "truncated").text = "0"
+                ET.SubElement(obj, "difficult").text = "0"
+
+                bbox = ET.SubElement(obj, "bndbox")
+                voc_box = box.to_voc().raw_values
+                ET.SubElement(bbox, "xmin").text = str(int(voc_box[0]))
+                ET.SubElement(bbox, "ymin").text = str(int(voc_box[1]))
+                ET.SubElement(bbox, "xmax").text = str(int(voc_box[2]))
+                ET.SubElement(bbox, "ymax").text = str(int(voc_box[3]))
+
+                tree.write(filepath) # after appending additional bounding box data, persist changes to the file
+
+            else: # the xml annotation doesn't exists, so we have to create file structure from the scratch
+                root = ET.Element("annotation")
+                ET.SubElement(root, "filename").text = annotation.associated_image_name
+                size = ET.SubElement(root, "size")
+                ET.SubElement(size, "width").text = str(annotation.image_width)
+                ET.SubElement(size, "height").text = str(annotation.image_height)
+                ET.SubElement(size, "depth").text = str(n_channels)
+
+                for box in annotation.boxes:
+                    obj = ET.SubElement(root, "object")
+                    ET.SubElement(obj, "name").text = annotation.label_name
+                    ET.SubElement(obj, "pose").text = "Unspecified"
+                    ET.SubElement(obj, "truncated").text = "0"
+                    ET.SubElement(obj, "difficult").text = "0"
+
+                    bbox = ET.SubElement(obj, "bndbox")
+                    voc_box = box.to_voc().raw_values
+                    ET.SubElement(bbox, "xmin").text = str(int(voc_box[0]))
+                    ET.SubElement(bbox, "ymin").text = str(int(voc_box[1]))
+                    ET.SubElement(bbox, "xmax").text = str(int(voc_box[2]))
+                    ET.SubElement(bbox, "ymax").text = str(int(voc_box[3]))
+
+                tree = ET.ElementTree(root)
+                tree.write(filepath)
+    
 if __name__ == "__main__":
     coco = Annotations(annotation_type='coco')
     coco.load_from_coco(json_path='Images/annotations_coco.json')
     coco.persist_as_yolo(export_dir='yolo_test')
+    # coco.persist_as_voc(export_dir='voc_test')
